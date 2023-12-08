12f3c47fdf4b625de22b8a2242bf67a98d10f677
stefanverleysen <stef@asan.digital>
2023-12-07 22:42:06 -0500

    falling him

--- static_code/script.js
+++ static_code/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   let timer;
   let isRunning = false;
   let countdownSeconds = 0;
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
   const thirtySecondsBtn = document.getElementById('thirtySeconds');
   const twoMinutesBtn = document.getElementById('twoMinutes');
   const alarmSoundSelector = document.getElementById('alarmSoundSelector');
+
+  // Array to store falling image elements
+  const fallingImages = [];
 
   function updateDisplay(minutes, seconds) {
     timerDisplay.innerText = pad(minutes) + ':' + pad(seconds);
@@ -35,8 +38,14 @@ document.addEventListener('DOMContentLoaded', function() {
       clearInterval(timer);
       isRunning = false;
       playSelectedAudio();
+      // Check if the selected audio is "Gentlemen's Voice"
+      const selectedOption = alarmSoundSelector.options[alarmSoundSelector.selectedIndex].value;
+      if (selectedOption === 'Audio/manvoice.mp3') {
+        // Initiate falling images animation
+        initiateFallingImagesAnimation();
+      }
     } else if (countdownSeconds <= 10) {
-      timerDisplay.classList.add('red-text', 'blink-text'); // Start blinking and turn red
+      timerDisplay.classList.add('red-text', 'blink-text');
     }
   }
 
@@ -58,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function() {
     timerDisplay.classList.remove('red-text', 'blink-text');
   }
 
-  startStopBtn.addEventListener('click', function() {
+  startStopBtn.addEventListener('click', function () {
     if (isRunning) {
       clearInterval(timer);
       isRunning = false;
@@ -70,19 +79,55 @@ document.addEventListener('DOMContentLoaded', function() {
 
   resetBtn.addEventListener('click', resetTimer);
 
-  thirtySecondsBtn.addEventListener('click', function() { startTimer(30); });
-  twoMinutesBtn.addEventListener('click', function() { startTimer(120); });
+  thirtySecondsBtn.addEventListener('click', function () {
+    startTimer(30);
+  });
+  twoMinutesBtn.addEventListener('click', function () {
+    startTimer(120);
+  });
 
   function pad(number) {
     return number < 10 ? '0' + number : number;
   }
 
   // Fullscreen functionality for the timer
-  fullscreenBtn.addEventListener('click', function() {
+  fullscreenBtn.addEventListener('click', function () {
     if (!document.fullscreenElement && timerDisplay.requestFullscreen) {
       timerDisplay.requestFullscreen();
     } else if (document.exitFullscreen) {
       document.exitFullscreen();
     }
   });
+
+  // Function to initiate falling images animation
+  function initiateFallingImagesAnimation() {
+    // Create and append falling image elements
+    for (let i = 0; i < 10; i++) {
+      const img = document.createElement('img');
+      img.src = 'images/jimtimesup.png';
+      img.classList.add('falling-image');
+      document.body.appendChild(img);
+      fallingImages.push(img);
+    }
+
+    // Start the falling animation
+    animateFallingImages();
+  }
+
+  // Function to animate falling images
+  function animateFallingImages() {
+    fallingImages.forEach((img, index) => {
+      const animationDuration = 5 + Math.random() * 5 + 's'; // Randomize animation duration
+      img.style.animation = `fall ${animationDuration} linear`;
+      img.style.left = Math.random() * window.innerWidth + 'px'; // Randomize horizontal position
+      img.style.opacity = '1'; // Make the image visible
+      img.style.transform = 'translateY(100vh)'; // Move the image to the bottom of the viewport
+
+      // Remove the image from the DOM after animation ends
+      img.addEventListener('animationend', () => {
+        img.remove();
+        fallingImages.splice(index, 1);
+      });
+    });
+  }
 });
