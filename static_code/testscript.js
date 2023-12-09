c9cd88fc4f6ea6321474bef79bdbdc5e02d3b1f4
stefanverleysen <stef@asan.digital>
2023-12-09 14:53:28 -0500

    test

--- static_code/testscript.js
+++ static_code/testscript.js
@@ -1,150 +1,130 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     let timer;
-  
     let isRunning = false;
-  
     let countdownSeconds = 0;
-  
+
     const timerDisplay = document.getElementById('timer');
-  
     const startStopBtn = document.getElementById('startStop');
-  
     const resetBtn = document.getElementById('reset');
-  
     const fullscreenBtn = document.getElementById('fullscreenBtn');
-  
     const tenSecondsBtn = document.getElementById('tenSeconds');
-  
     const thirtySecondsBtn = document.getElementById('thirtySeconds');
-  
     const twoMinutesBtn = document.getElementById('twoMinutes');
-  
     const alarmSoundSelector = document.getElementById('alarmSoundSelector');
-  
+    const fsContainer = document.getElementById('fullscreen-container'); // Fullscreen container
+
     function updateDisplay(minutes, seconds) {
-      timerDisplay.innerText = pad(minutes) + ':' + pad(seconds); 
+        timerDisplay.innerText = pad(minutes) + ':' + pad(seconds); 
     }
-  
+
     function startTimer(duration) {
-      countdownSeconds = duration;
-      
-      if (!isRunning) {
-        timer = setInterval(updateTime, 1000);
-        isRunning = true;
-      }
-      
-      resetColor();
+        countdownSeconds = duration;
+        if (!isRunning) {
+            timer = setInterval(updateTime, 1000);
+            isRunning = true;
+        }
+        resetColor();
     }
-  
+
     function updateTime() {
-      let minutes = Math.floor(countdownSeconds / 60);
-      let seconds = countdownSeconds % 60;
-  
-      updateDisplay(minutes, seconds);
-  
-      countdownSeconds--;
-  
-      if (countdownSeconds < 0) {
-        clearInterval(timer);
-        isRunning = false;
-        playSelectedAudio();
-        initiateFallingImagesAnimation();
-      } else if (countdownSeconds <= 10) {
-        timerDisplay.classList.add('red-text', 'blink-text');
-      }
+        let minutes = Math.floor(countdownSeconds / 60);
+        let seconds = countdownSeconds % 60;
+
+        updateDisplay(minutes, seconds);
+        countdownSeconds--;
+
+        if (countdownSeconds < 0) {
+            clearInterval(timer);
+            isRunning = false;
+            playSelectedAudio();
+            initiateFallingImagesAnimation();
+        } else if (countdownSeconds <= 10) {
+            timerDisplay.classList.add('red-text', 'blink-text');
+        }
     }
-  
+
     function resetTimer() {
-      clearInterval(timer);
-      updateDisplay(0, 0);
-      isRunning = false;
-      countdownSeconds = 0;
-      resetColor();
-    }
-  
-    function playSelectedAudio() {
-      const selectedOption = alarmSoundSelector.options[alarmSoundSelector.selectedIndex].value;
-      const audio = new Audio(selectedOption);
-      audio.play();
-    }
-  
-    function resetColor() {
-      timerDisplay.classList.remove('red-text', 'blink-text');
-    }
-  
-    startStopBtn.addEventListener('click', function() {
-      if (isRunning) {
         clearInterval(timer);
+        updateDisplay(0, 0);
         isRunning = false;
-        resetColor(); 
-      } else {
-        startTimer(countdownSeconds > 0 ? countdownSeconds : 60);
-      }
-    });
-  
-    resetBtn.addEventListener('click', resetTimer);
-  
-    tenSecondsBtn.addEventListener('click', function() {
-      startTimer(10); 
-    });
-  
-    thirtySecondsBtn.addEventListener('click', function() {
-      startTimer(30);
-    });
-  
-    twoMinutesBtn.addEventListener('click', function() {
-      startTimer(120); 
-    });
-  
-    function pad(number) {
-      return number < 10 ? '0' + number : number;
+        countdownSeconds = 0;
+        resetColor();
     }
-  
-    // Fullscreen functionality for the timer
-  
-    fullscreenBtn.addEventListener('click', function() {
-      if (!document.fullscreenElement && timerDisplay.requestFullscreen) {
-        timerDisplay.requestFullscreen();
-      } else if (document.exitFullscreen) {  
-        document.exitFullscreen();
-      }
+
+    function playSelectedAudio() {
+        const selectedOption = alarmSoundSelector.options[alarmSoundSelector.selectedIndex].value;
+        const audio = new Audio(selectedOption);
+        audio.play();
+    }
+
+    function resetColor() {
+        timerDisplay.classList.remove('red-text', 'blink-text');
+    }
+
+    startStopBtn.addEventListener('click', function() {
+        if (isRunning) {
+            clearInterval(timer);
+            isRunning = false;
+            resetColor(); 
+        } else {
+            startTimer(countdownSeconds > 0 ? countdownSeconds : 60);
+        }
     });
-  
+
+    resetBtn.addEventListener('click', resetTimer);
+
+    tenSecondsBtn.addEventListener('click', function() {
+        startTimer(10); 
+    });
+
+    thirtySecondsBtn.addEventListener('click', function() {
+        startTimer(30);
+    });
+
+    twoMinutesBtn.addEventListener('click', function() {
+        startTimer(120); 
+    });
+
+    function pad(number) {
+        return number < 10 ? '0' + number : number;
+    }
+
+    // Fullscreen toggle functionality
+    fullscreenBtn.addEventListener('click', function() {
+        if (!document.fullscreenElement) {
+            if (fsContainer.requestFullscreen) {
+                fsContainer.requestFullscreen();
+            }
+        } else {
+            if (document.exitFullscreen) {  
+                document.exitFullscreen();
+            }
+        }
+    });
+
     function initiateFallingImagesAnimation() {
-      const selectedOption = alarmSoundSelector.options[alarmSoundSelector.selectedIndex].value;
-  
-      // Check if the selected audio is "General Moore"
-  
-      if (selectedOption === 'Audio/manvoice.mp3') {
+        const selectedOption = alarmSoundSelector.options[alarmSoundSelector.selectedIndex].value;
+
+        if (selectedOption === 'Audio/manvoice.mp3') {
+            createAndAnimateImage('images/jimtimesup.png');
+        } else if (selectedOption === 'Audio/wolf5.mp3') {
+            createAndAnimateImage('images/wolf.png');
+        }
+        // Add more conditions for other audio selections here if needed
+    }
+
+    function createAndAnimateImage(imageSrc) {
         const img = document.createElement('img');
-        img.src = 'images/jimtimesup.png'; // Image for General Moore  
-        img.classList.add('falling-image');
-        document.body.appendChild(img);
-  
-        setTimeout(() => {
-          img.style.animation = 'growAndShrink 10s forwards'; // CSS animation name  
-        }, 100);  // Short delay
-  
-        img.addEventListener('animationend', () => {
-          img.remove(); // Removes the image after animation  
-        });
-      }
-      
-     // Check if the selected audio is "Wolf Howl"
-      else if (selectedOption === 'Audio/wolf5.mp3') {
-        const img = document.createElement('img');
-        img.src = 'images/wolf.png'; // Image for Wolf Howl
+        img.src = imageSrc;
         img.classList.add('falling-image');
         document.body.appendChild(img);
 
         setTimeout(() => {
-          img.style.animation = 'growAndShrink 10s forwards'; // CSS animation name
-        }, 100); // Short delay
+            img.style.animation = 'growAndShrink 10s forwards';
+        }, 100); 
 
         img.addEventListener('animationend', () => {
-          img.remove(); // Removes the image after animation
+            img.remove();
         });
-  }
-    }  
-  });
+    }
+});
