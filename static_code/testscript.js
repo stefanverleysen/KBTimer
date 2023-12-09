7ab7eb707079ca487872a723fa00852816d85eaf
stefanverleysen <stef@asan.digital>
2023-12-09 15:09:27 -0500

    lets go

--- static_code/testscript.js
+++ static_code/testscript.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let timer;
     let isRunning = false;
     let countdownSeconds = 0;
+    let sessionStartTime;
+    let lapTimes = [];
 
     const timerDisplay = document.getElementById('timer');
     const startStopBtn = document.getElementById('startStop');
@@ -10,6 +12,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const tenSecondsBtn = document.getElementById('tenSeconds');
     const thirtySecondsBtn = document.getElementById('thirtySeconds');
     const twoMinutesBtn = document.getElementById('twoMinutes');
+    const startKbSessionBtn = document.getElementById('startKbSession');
+    const addTenSecondsBtn = document.getElementById('addTenSeconds');
+    const addThirtySecondsBtn = document.getElementById('addThirtySeconds');
+    const recordLapBtn = document.getElementById('recordLap');
+    const lapRecordsTable = document.getElementById('lap-records');
     const alarmSoundSelector = document.getElementById('alarmSoundSelector');
     const fsContainer = document.getElementById('fullscreen-container'); // Fullscreen container
 
@@ -61,6 +68,7 @@ document.addEventListener('DOMContentLoaded', function() {
         timerDisplay.classList.remove('red-text', 'blink-text');
     }
 
+    // Event listeners for the timer buttons
     startStopBtn.addEventListener('click', function() {
         if (isRunning) {
             clearInterval(timer);
@@ -74,16 +82,51 @@ document.addEventListener('DOMContentLoaded', function() {
     resetBtn.addEventListener('click', resetTimer);
 
     tenSecondsBtn.addEventListener('click', function() {
-        startTimer(10); 
+        addTime(10);
     });
 
     thirtySecondsBtn.addEventListener('click', function() {
-        startTimer(30);
+        addTime(30);
     });
 
     twoMinutesBtn.addEventListener('click', function() {
         startTimer(120); 
     });
+
+    startKbSessionBtn.addEventListener('click', function() {
+        sessionStartTime = new Date();
+        lapTimes = [];
+        startTimer(60); // Start with a default duration
+        updateLapRecordsTable();
+    });
+
+    addTenSecondsBtn.addEventListener('click', function() {
+        addTime(10);
+    });
+
+    addThirtySecondsBtn.addEventListener('click', function() {
+        addTime(30);
+    });
+
+    recordLapBtn.addEventListener('click', function() {
+        const lapTime = Math.floor((new Date() - sessionStartTime) / 1000);
+        lapTimes.push(lapTime);
+        updateLapRecordsTable();
+    });
+
+    function addTime(seconds) {
+        countdownSeconds += seconds;
+        updateDisplay(Math.floor(countdownSeconds / 60), countdownSeconds % 60);
+    }
+
+    function updateLapRecordsTable() {
+        lapRecordsTable.innerHTML = ''; // Clear existing records
+        lapTimes.forEach((time, index) => {
+            const row = lapRecordsTable.insertRow(0); // Insert at the top
+            const cell = row.insertCell(0);
+            cell.textContent = `Lap ${index + 1}: ${time} seconds`;
+        });
+    }
 
     function pad(number) {
         return number < 10 ? '0' + number : number;
@@ -102,6 +145,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // Falling images animation based on audio selection
     function initiateFallingImagesAnimation() {
         const selectedOption = alarmSoundSelector.options[alarmSoundSelector.selectedIndex].value;
 
